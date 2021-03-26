ce68c087cc06ac0eace92bad7f2d89b7af82dd65
ekoeryanto <ekoeryanto@gmail.com>
2021-03-26 22:11:24 +0700

    chore: partial import sass

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import quasarMap from 'quasar/dist/transforms/import-map.json'
-import VitePluginComponents from 'vite-plugin-components'
+import ViteComponents from 'vite-plugin-components'
+import styleImport from 'vite-plugin-style-import';
+import {pascalCase} from 'pascal-case'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'quasar.variables.sass': resolve(__dirname, './src/assets/quasar.variables.sass')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: '@import "quasar.variables.sass"\n'
+      }
+    }
+  },
   define: {
     __QUASAR_VERSION__: JSON.stringify('2.0.0'),
     __QUASAR_SSR__: false,
@@ -23,11 +38,24 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    VitePluginComponents({
+    ViteComponents({
       customComponentResolvers: [
         name => quasarMap[name] && ({
           importName: name, path: 'quasar'
         })
+      ]
+    }),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'quasar',
+          esModule: true,
+          resolveStyle: (name) => {
+            const pname = pascalCase(name)
+            return 'quasar/'+quasarMap[pname].replace(/.js$/, '.sass')
+          },
+          ensureStyleFile: true
+        }
       ]
     })
   ]
